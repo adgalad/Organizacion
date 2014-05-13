08e65035d51f102900012c49ce421abe0388d990
adgalad <carlos.25896@gmail.com>
2014-05-12 19:57:31 -0430

    Montados en el venoso

--- ProyectoOrganizacion.asm
+++ ProyectoOrganizacion.asm
@@ -5,6 +5,8 @@
 FAT:		.space  512  # 256 bytes reservados para la Tabla FAT
 Directorio:	.space  256  # 512 casillas de 2 bytes (nombre y cluster de inicio) reservados para el Directorio 
 DiscoDuro:	.space  1024 # 1024 bytes (1 kb) reservados para el disco 
+Buffer:		.space  40   # Espacio reservado para el buffer del input
+
 
 Error1:		.asciiz "Error: El Disco esta lleno\n"
 Error2:		.asciiz "Error: El Archivo que desea crear ya existe en el Disco\n"
@@ -26,9 +28,18 @@ Prompt:		.asciiz ">> "
 
 	imprime(Bienvenida)
 loop:	imprime(Prompt)
-	#
-	# Reconocer funcion
-	#
+	
+	la $v0 , 8
+	la $a0 , Buffer
+	li $a1 , 40
+	syscall
+	
+	lw $a1 , 0($a0)
+	beq $a1 , 1634038371 , crear
+	beq $a1 , 1886547305 , imprimir
+	beq $a1 , 1768976227 , copiar
+	beq $a1 , 544105842  , ren
+	beq $a1 , 1702521203 , sizeof
 	
 crear:
 
