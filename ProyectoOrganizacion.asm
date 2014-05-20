7e5e1e1dc63721d533e8d17bbd05c9e01efe6ab5
adgalad <carlos.25896@gmail.com>
2014-05-19 23:29:28 -0430

    no se si hice algun cambio, pero comiteo igual

--- ProyectoOrganizacion.asm
+++ ProyectoOrganizacion.asm
@@ -1,17 +1,25 @@
 
 
 	.data
-	
+.align 2	
 FAT:		.space  256  # 256 bytes reservados para la Tabla FAT
+.align 2
 directorio:	.space  3584 # 256 casillas de 14 bytes (13 para nombre y uno para cluster de inicio)
+.align 2
 discoDuro:	.space  1024 # 1024 bytes (1 kb) reservados para el disco 
+.align 2
 buffer:		.space  1025 # Espacio reservado para el buffer del input (Maximo 1 Kb + 1)
+.align 2
 bufferIO:	.space	1025 # Espacio reservado para el buffer del IO de archivos
+
 
 error1:		.asciiz "Error: El Disco esta lleno\n"
 error2:		.asciiz "Error: El Archivo que desea crear ya existe en el Disco\n"
 error3:		.asciiz "Error: El Archivo no existe en el Disco\n"
 error4:		.asciiz "Error: Comando invalido\n"
+error5:		.asciiz "Error: No hay espacio suficiente\n"
+error6:		.asciiz "Error: El nombre del archivo es muy largo\n"
+error7:		.asciiz "Error: El nombre al que quieres renombrar ya existe \n"
 
 bienvenida:	.asciiz "   Sistema Manejador de Disco Duro (SMD)\n"
 prompt:		.asciiz ">> "
@@ -21,6 +29,7 @@ textCopiar:	.asciiz "copiar"
 textRenombrar:	.asciiz "ren"
 textSizeOf:	.asciiz "sizeof"
 textImprimir:	.asciiz "imprimir"
+textBuscar:	.asciiz "buscar"
 textSalir:	.asciiz "salir"
 	
 	
@@ -33,8 +42,11 @@ textSalir:	.asciiz "salir"
 	syscall
 .end_macro
 
-
-
+# inicializo la casilla 0 de FAT en 255 ( el total de clusters libres )
+		addi $t0, $0, 255
+		la $t1, FAT
+		sb  $t0, 0($t1)
+		
 		imprime(bienvenida)
 main:		imprime(prompt)
 
@@ -71,12 +83,24 @@ ifCopiar:	la   $a1, textCopiar
 ifRen:		la   $a1, textRenombrar
 		move $a0, $s0
 		jal  compararString
-		beqz $v0, ifSizeOf 
+		beqz $v0, ifSizeOf
+		jal ren
+		b main 
 		
 ifSizeOf:	la   $a1, textSizeOf
 		move  $a0, $s0
 		jal  compararString
-		beqz $v0, ifSalir 
+		beqz $v0, ifBuscar
+		jal sizeof
+		b main 
+
+ifBuscar:	la   $a1, textBuscar
+		move  $a0, $s0
+		jal  compararString
+		beqz $v0, ifSalir
+		move  $a0, $s1
+		jal buscar
+		b main 	
 		
 ifSalir:	la   $a1, textSalir
 		move $a0, $s0
@@ -85,7 +109,8 @@ ifSalir:	la   $a1, textSalir
 		
 		imprime(error4)
 		b main
-		
+
+			
 	
 		
 salirMain:      li  $v0, 10
@@ -140,8 +165,21 @@ salirInput:	add  $t0, $0, $0
 # Entrada: $a0 ( direccion con el nombre del archivo )
 # Salida:  nada
 
-crear:		move $v1, $s1
+# Comandos
+
+# Entrada:
+# Salida:	
+
+crear:		addi $sp, $sp, -8
+		sw $fp, 8($sp)
+		sw $ra, 4($sp)
+		addi $fp, $sp, 8
+		move $a0, $s1
 		jal split
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 8
+		
 		li   $v0, 13
 		move $a0, $s1
 		li   $a1, 0
@@ -153,26 +191,461 @@ crear:		move $v1, $s1
 		la   $a1, bufferIO
 		li   $a2, 1025
 		syscall
-		
-		imprime(bufferIO)
-		
-		li   $v0, 16
+				
+		li $v0, 16
 		syscall
 		
-		jr   $ra
+		add $t3, $0, $0
+		add $t5, $0, $0
+		add $t0, $0, $0
+		move $t3, $s1
+		
+tamanonombre:	lb $t5, 0($t3)		# Verifico si el nombre del archivo no excede los 12 caracteres
+		beqz $t5, cheqnombre
+		addi $t0, $t0, 1
+		addi $t3, $t3, 1
+		b tamanonombre
+		
+CError6:	imprime(error6)
+		jr $ra
+CError2:	imprime(error2)
+		jr $ra
+		
+cheqnombre:	addi $t5, $t5, 12
+		bgt $t0, $t5, CError6		
+
+
+		add $t3, $0, $0		# Chequeo si existe el nombre en el directorio
+		add $t5, $0, $0
+		addi $t5, $t5, 256
+		la $t0, directorio
+		move $a1, $s1
+		
+cheqdirect:	la $a0, 0($t0) 		
+		addi $sp, $sp, -12
+		sw $fp, 12($sp)
+		sw $ra, 8($sp)
+		sw $t0, 4($sp)
+		addi $fp, $sp, 12
+		jal compararString
+		lw $t0, -8($fp)
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 12
+		addi $t3, $t3, 1
+		addi $t0, $t0, 14
+		bgtz $v0, CError2
+		blt $t3, $t5, cheqdirect
+		
+		
+		add $t3, $0, $0		# Cuento el numero de bytes a guardar
+		la $t0, bufferIO
+		
+contandopal:	addi $t0, $t0, 1
+		lb $t1, 0($t0)
+		beqz $t1, espaciolibre
+		addi $t3, $t3, 1
+		b contandopal
+		
+CError5:	imprime(error5)
+		jr $ra
+		 
+espaciolibre:	beqz $t3, salircrear	# Verifico si hay espacio suficiente
+		la $t4, FAT
+		lbu  $t4, 0($t4)
+		add $t5, $0, $0
+		addi $t5, $t5, 4
+		mul $t4, $t4, $t5
+		bgt $t3, $t4, CError5
+		
+		
+		la $t0, bufferIO
+		la $t4, FAT		# El FAT 0, esta reservado para uso del SMD
+		la $t5, discoDuro		
+		add $t2, $0, $0
+		b clusterlibres
+
+		
+clusterlibres1:	move $t9, $t4		# Encuentro Clusters libres para almacenar
+		addi $t4, $t4, 1			
+clusterlibres:	lb $t1, 0($t4)
+		beqz $t1, clusterlibre
+		addi $t4, $t4, 1	
+		b clusterlibres
+		
+clusterlibre:	bnez $t2, marcandofat	# Calculo la biyeccion entre el indice de FAT y el HDD
+clusterlibre1:	la $t6, FAT
+		sub $t6, $t4, $t6
+		mul $t6, $t6, 4
+		add $t6, $t5, $t6
+		
+		add $t8, $0, $0		# Guardo 4bytes en un cluster
+guardando:	lb $t7, 0($t0)
+		sb $t7, 0($t6)
+		addi $t0, $t0, 1
+		addi $t6, $t6, 1
+		addi $t2, $t2, 1
+		addi $t8, $t8, 1
+		beq $t3, $t2, marcandofatf
+		beq $t8, 4, clusterlibres1
+		b guardando
+	
+marcandofat:	ble $t2, 4, entradadirect	# Identifico en el cluster de FAT, el proximo cluster de la plabra
+marcandofat1:	la $t6, FAT
+		sub $t6, $t4, $t6
+		sb $t6, 0($t9)
+		b clusterlibre1
+		
+entradadirect:	la $t6,	directorio		# Creo la entrada en el directorio
+entradadirect1:	lb $t7, 0($t6)
+		beqz $t7, llenardirect
+		addi $t6, $t6, 14
+		b entradadirect1
+		
+llenardirect:	addi $sp, $sp, -4
+		sw $t6, 4($sp)
+		move $t7, $s1
+llenardirect1:	lb $t8, 0($t7)
+		beqz $t8, llenardirect2
+		sb $t8, 0($t6)
+		addi $t7,$t7, 1
+		addi $t6, $t6, 1
+		b llenardirect1
+		
+llenardirect2:	lw $t7, 4($sp)
+		addi $sp, $sp, 4
+		addi $t7, $t7, 13
+		la $t6, FAT
+		sub $t6, $t9, $t6
+		sb $t6, 0($t7)
+		b marcandofat1
+		
+		
+marcandofatf:	subi $t6, $0, 1		# Marco -1 en el cluster en FAT para el ultimo cluster usado.
+		sb $t6, 0($t4)
+				    
+salircrear:	jr   $ra
+
+
+
 
 # Entrada: $a0 ( direccion con el nombre del archivo )
 # Salida:  nada
-imprimir: 	li   $v0 4
-		la   $a0, 0($s1)
-		syscall
+imprimir: 	li   $t0, 0
+		la   $t1, directorio
+
+		# En loopExisteImp:		
+		# $t0 iterador
+		# $t1 direccion del directorio
+		# $a0 direccion con nombre del archivo
 		
+loopExisteImp:	addi $sp, $sp, -8
+		sw   $ra, 4($sp)
+		sw   $t1, 8($sp)
+		move $a1, $t1
+		jal  split
+		move $a0, $v0
+		jal  compararString
+		lw   $ra, 4($sp)
+		lw   $t1, 8($sp)
+		addi $sp, $sp, 8
+		beq  $v0, 1 , existeImpri
+		beq  $t0, 255, noExisteImpri
+		la   $t1, directorio
+		addi $t1, $t1, 14			# flag
+		addi $t0, $t0, 1
+		b loopExisteImp
+
+existeImpri:    la   $t0 bufferIO
+		addi $t1, $t1, 13 			# flag
+		
+		# En loopImpri:
+		# $t0 variable para moverse y escribir el bufferIO
+		# $t1 direccion con la FAT
+		# $t2 direccion del disco duro
+		# $t3 variable para moverse en el Disco y la FAT
+		
+loopImpri:	la   $t2 discoDuro	
+		lb   $t3, 0($t1)
+		la   $t1, FAT
+		add  $t1, $t1, $t3
+		lb   $t3, 0($t1) 
+		beqz $t3, salirImpri
+		sll  $t3, $t3, 2
+		add  $t2, $t2, $t3
+		lw   $t3, 0($t2)
+		sw   $t3, 0($t0)
+		addi $t0, $t0, 4
+		b loopImpri
+
+salirImpri:	imprime(bufferIO)
+		jr $ra		
+
+noExisteImpri:  imprime(error3)
 		jr   $ra
 
+
+
+
+# Entrada: 
+# Salida: 
 copiar:		b main
 
-ren:		b main
 
-sizeof:		b main
+# Entrada: 
+# Salida: 
+ren:		move $a0, $s1		# Divide los dos nombres de archivo en s1
+		addi $sp, $sp, -8
+		sw $fp, 8($sp)
+		sw $ra, 4($sp)
+		addi $fp, $sp, 8
+		jal split
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 8
+		move $t8, $v0
+		move $t9, $v1
+		
+		move $a0, $t9		# Elimino el salto de linea al final del argumento
+		addi $sp, $sp, -8	
+		sw $fp, 8($sp)
+		sw $ra, 4($sp)
+		addi $fp, $sp, 8
+		jal split
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 8
+		
+		la $t0, directorio
+		addi $t5, $0, 256
+		move $a1, $t9
+		add $t3, $0, $0
+		
+cheqdirect1:	la $a0, 0($t0)		# Chequeo si el nombre a renombrar, exista en el directorio
+		addi $sp, $sp, -12
+		sw $fp, 12($sp)
+		sw $ra, 8($sp)
+		sw $t0, 4($sp)
+		addi $fp, $sp, 12
+		jal compararString
+		lw $t0, -8($fp)
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 12
+		bgtz $v0, renError7
+		addi $t3, $t3, 1
+		addi $t0, $t0, 14
+		blt $t3, $t5, cheqdirect1
+		
+		add $t2, $0, $0
+		move $t0, $t9		# verifico si el nombre a cambiar tiene 12 chars o menos
+rencuentachar:	lb $t1, 0($t0)
+		beqz $t1, verificanum
+		addi $t0, $t0, 1
+		addi $t2, $t2, 1
+		b rencuentachar
+		
+renError7:	imprime(error7)
+		jr $ra
+
+renError6:	imprime(error6)
+		jr $ra
+					
+verificanum:	bge $t2, 13, renError6		
+		
+		la $t0, directorio
+		addi $t5, $0, 256
+		move $a1, $t8
+		add $t3, $0, $0
+		
+cheqdirect2:	la $a0, 0($t0) 		# Busco el archivo en el directorio
+		addi $sp, $sp, -16
+		sw $fp, 16($sp)
+		sw $ra, 12($sp)
+		sw $t0, 8($sp)
+		sw $a1, 4($sp)
+		addi $fp, $sp, 16
+		jal compararString
+		lw $a1, -12($fp)
+		lw $t0, -8($fp)
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 12
+		move $t7, $t0
+		bgtz $v0, limpionombre
+		addi $t3, $t3, 1
+		addi $t0, $t0, 14
+		beq  $t3, $t5, renError3
+		b cheqdirect2
+
+renError3:	imprime(error3)		
+		
+limpionombre:	add $t6, $0, $0
+		sb $t6, 0($t0)
+		addi $t0, $t0, 1
+		lb $t5, 0($t0)
+		beqz $t5, renombro
+		b limpionombre
+		
+renombro:	lb $t6, 0($t9)
+		beqz $t6, rensalir
+		sb $t6, 0($t7)
+		addi $t9, $t9, 1
+		addi $t7, $t7, 1
+		b renombro
+		
+rensalir:	jr $ra	
+	
+			
+		
+		
+# Entrada: $a0 ( Direccion con el nombre del string a buscar)
+# Salida: $v0 ( Nombre del archivo que contiene el string buscado)	
+
+
+		# $t0 directorio
+		# $t1 string
+		# $t2 disco duro
+		# $t3 FAT
+		# $t4 auxiliar
+		# $t5 iterador
+		# $t6 iterador
+		
+buscar:		li   $t6, 0
+		addi $sp, $sp, -4
+		sw   $ra, 4($sp)
+		jal  split
+		lw   $ra, 4($sp)
+		addi $sp, $sp, 4
+loopBuscarDir:	la   $t0, directorio
+		li   $t1, 14
+		mult $t1, $t6, 
+		add  $t0, $t0, $t1
+		addi $t0, $t0, 13
+		move $t1, $a0
+		lb   $t4, 0($t0)
+
+
+loopBuscarFAT:	la   $t2, discoDuro
+		sll  $t5, $t4, 2
+		add  $t2, $t2, $t4 
+		li   $t5, 0
+
+loopComparar:   lb   $t4, 0($t2)
+		lb   $t7, 0($t1)
+		bne  $t4, $t7, salirLoop
+		addi $t5, $t5, 1
+		addi $t2, $t2, 1
+		addi $t1, $t1, 1
+		bne  $t5, 4, loopComparar
+		
+		la  $t0, FAT
+		add $t0, $t0, $t4
+		lb  $t4, 0($t0)
+		beq $t4, $0, retornarBuscar
+		b loopBuscarFAT
+
+salirLoop:	addi $t6, $t6, 1
+		blt  $t6, 256, loopBuscarDir      
+		li   $v0, -1
+		jr   $ra
+		
+retornarBuscar:	li   $t0, 14
+		mult $t6, $t0
+		la   $v0, directorio
+		add  $v0, $v0, $t6
+		jr   $ra
+				
+		
+		
+
+
+#Entrada: $s1 ( nombre del archivo )
+#Salida:   ( Unidades de cluster ) y ( Unidades de Bytes ) 
+
+sizeof:		move $t9, $s1		# Elimino el salto de linea al final del argumento
+		move $a0, $t9
+		addi $sp, $sp, -8
+		sw $fp, 8($sp)
+		sw $ra, 4($sp)
+		addi $fp, $sp, 8
+		jal split
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 8
+		
+		
+		add $t0, $0, $0		# Chequeo si el archivo existe
+		la $t1, directorio
+		add $t2, $0, $0
+		addi $t3, $0, 256
+		move $a1, $t9
+		
+cheqdirect3:	la $a0, 0($t1)
+		addi $sp, $sp, -20
+		sw $fp, 20($sp)
+		sw $ra, 16($sp)
+		sw $a1, 12($sp)
+		sw $t0, 8($sp)
+		sw $t1, 4($sp)
+		addi $fp, $sp, 20
+		jal compararString
+		lw $t1, -16($fp)
+		lw $t0, -12($fp)
+		lw $a1, -8($fp)
+		lw $ra, -4($fp)
+		lw $fp, 0($fp)
+		addiu $sp, $sp, 20
+		move $t0, $t1
+		bgtz $v0, calculator
+		addi $t2, $t2, 1
+		addi $t1, $t1, 14
+		beq $t2, $t3, sizeofError3
+		b cheqdirect3
+		
+sizeofError3:	imprime(error3)
+		jr $ra
+		
+calculator:	add $t6, $0, $0
+		lbu $t1, 13($t0)	# Realizo la biyeccion del FAT al DiscoDuro
+calculator1:	la $t0, FAT
+		la $t3,	discoDuro
+		add $t0, $t0, $t1	
+		mul $t2, $t1, 4
+		add $t3, $t3, $t2
+		
+cuentacluster:	add $t4, $0, $0
+		lb $t5, 0($t3)
+		beqz $t5, etiqueta
+		addi $t4, $t4, 1
+		addi $t6, $t6, 1
+		addi $t3, $t3, 1
+		blt $t4, 4, cuentacluster
+		lbu $t1, 0($t0)
+		b calculator1
+		
+etiqueta:	li $v0, 1
+		move $a0, $t6
+		syscall
+		
+		jr $ra
+		
+		 
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 
